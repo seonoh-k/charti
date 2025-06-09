dbf47175683b0732ea3708326d28b0415c910199
Jisu12411 <parkjisu05@naver.com>
2025-06-09 17:12:22 +0900

    데일리문진 기능 구현 - DailySurvey 엔티티 생성 - DailySurveyRepository 생성 - DailySurveyService 생성   -> 응답 기반 위험도 계산(100점 만점) - DailySurveyController 생성   -> DB에서 연령별 문진 데이터 가져오기   -> 문진 데이터 null, 답변 null 등 예외처리   -> 결과 페이지 라우팅 추가 - SurveyRequestDto 생성   -> 필요한 데이터(연령과 답변)만 입력받음 - MainController에 데일리 문진 페이지, 데일리 문진 결과 페이지 연결 - DailySurvey.html 생성   -> 데일리 문진 페이지 - DailySurveyResult.html 생성   -> 데일리 문진 결과 페이지

---
+++ src/main/java/com/example/demo/dto/SurveyRequestDto.java
@@ -0,0 +1,14 @@
+package com.example.demo.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+@Getter
+@Setter
+@NoArgsConstructor
+public class SurveyRequestDto {
+    private String ageGroup;
+    private List<Integer> answers;
+}
